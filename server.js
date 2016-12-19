15ac809d27d234ad89dfeee11e096e94a768cebf
Nick Denys <nickdenys@gmail.com>
2016-12-19 22:55:56 +0100

    [FEATURE] You can now add items to your grocery list.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const sqlite = require('sqlite3').verbose()
-const db = new sqlite.Database('./data/database.sqlite')
+let db = new sqlite.Database('./data/database.sqlite')
 const Promise = require('bluebird')
 const Slapp = require('slapp')
 const ConvoStore = require('slapp-convo-beepboop')
@@ -19,40 +19,96 @@ var slapp = Slapp({
 })
 
 //*********************************************
-// Grocery-Bot
+// Database
 //*********************************************
-
-slapp.command('/grocery', (msg, text) => {
-  if (!text) {
-    msg.respond("Whoops. Try again.")
-  } else {
-    msg.respond("Alright! We've added it to the list.")
-  }
-})
 
 function fetchList() {
   return new Promise(function(resolve, reject) {
-    db.serialize(function() {
+    db = new sqlite.Database('./data/database.sqlite')
+    db.serialize(() => {
       db.all("SELECT * FROM Groceries", function(err, rows) {
         if (err) {
-          var responseObj = {
+          console.log(err)
+          let responseObj = {
             'error': err
           }
           reject(responseObj)
-          db.close()
         } else {
-          var responseObj = {
+          let responseObj = {
             'groceries': rows
           }
           resolve(responseObj)
-          db.close()
         }
       })
     })
+    db.close()
   })
 }
 
-slapp.command('/list', (msg) => {
+function addToList(text) {
+  return new Promise(function(resolve, reject) {
+    db = new sqlite.Database('./data/database.sqlite')
+    db.serialize(() => {
+      var stmt = db.prepare("INSERT INTO Groceries (name) VALUES (?)")
+      stmt.run(text, function(err) {
+        if (err) {
+          console.log(err)
+          let responseObj = {
+            'error': err
+          }
+          reject(responseObj)
+        } else {
+          let responseObj = {
+            'item': this
+          }
+          resolve(responseObj)
+        }
+      })
+      stmt.finalize()
+    })
+    db.close()
+  })
+}
+
+//*********************************************
+// Commands
+//*********************************************
+
+slapp.command('/grocery', 'help', (msg) => {
+  var HELP_TEXT = `
+  I will respond to the following messages:
+  \`help\` - to see this message.
+  \`list\` - to see all items on the grocery list.
+  \`add [item]\` - to add an item to the list.
+  \`remove [id]\` - to remove an item from the list.
+  \`clear\` - to remove all items from the list and start from scratch.
+  `
+  msg.say(HELP_TEXT)
+})
+
+slapp.command('/grocery', 'add (.*)', (msg, text, item) => {
+  if (!text) {
+    msg.respond("Whoops. Try again.")
+  } else {
+    try {
+      addToList(item)
+        .then((response) => {
+          if(response.error) {
+            console.log(response.error)
+            msg.respond("Something went wrong. We couldn't add that to the list :scream:")
+          } else {
+            console.log(response.item)
+            msg.respond("Alright! We've added it to the list.")
+          }
+        })
+    } catch (err) {
+      msg.respond("Something went wrong. We couldn't add that to the list :sweat:")
+      next(err)
+    }
+  }
+})
+
+slapp.command('/grocery', 'list', (msg) => {
   try {
     let groceries = null
     let groceries_text = ``
@@ -61,6 +117,7 @@ slapp.command('/list', (msg) => {
       .then((response) => {
         if(response.error) {
           console.log(response.error)
+          msg.respond("Something went wrong. We can't seem to find your list :anguished:")
         } else {
           groceries = response.groceries
           for(var i=0; i<groceries.length; i++) {
@@ -74,9 +131,8 @@ slapp.command('/list', (msg) => {
           msg.respond(groceries_text)
         }
       })
-
-    // msg.say(groceries_pretext)
   } catch (err) {
+    msg.respond("Something went wrong. We can't seem to find your list :anguished:")
     next(err)
   }
 })
@@ -85,98 +141,98 @@ slapp.command('/list', (msg) => {
 // Example functions
 //*********************************************
 
-var HELP_TEXT = `
-I will respond to the following messages:
-\`help\` - to see this message.
-\`hi\` - to demonstrate a conversation that tracks state.
-\`thanks\` - to demonstrate a simple response.
-\`<type-any-other-text>\` - to demonstrate a random emoticon response, some of the time :wink:.
-\`attachment\` - to see a Slack attachment message.
-`
-
-// response to the user typing "help"
-slapp.message('help', ['mention', 'direct_message'], (msg) => {
-  msg.say(HELP_TEXT)
-})
-
-// "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
-slapp
-  .message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
-    msg
-      .say(`${text}, how are you?`)
-      // sends next event from user to this route, passing along state
-      .route('how-are-you', { greeting: text })
-  })
-  .route('how-are-you', (msg, state) => {
-    var text = (msg.body.event && msg.body.event.text) || ''
-
-    // user may not have typed text as their next action, ask again and re-route
-    if (!text) {
-      return msg
-        .say("Whoops, I'm still waiting to hear how you're doing.")
-        .say('How are you?')
-        .route('how-are-you', state)
-    }
-
-    // add their response to state
-    state.status = text
-
-    msg
-      .say(`Ok then. What's your favorite color?`)
-      .route('color', state)
-  })
-  .route('color', (msg, state) => {
-    var text = (msg.body.event && msg.body.event.text) || ''
-
-    // user may not have typed text as their next action, ask again and re-route
-    if (!text) {
-      return msg
-        .say("I'm eagerly awaiting to hear your favorite color.")
-        .route('color', state)
-    }
-
-    // add their response to state
-    state.color = text
-
-    msg
-      .say('Thanks for sharing.')
-      .say(`Here's what you've told me so far: \`\`\`${JSON.stringify(state)}\`\`\``)
-    // At this point, since we don't route anywhere, the "conversation" is over
-  })
-
-// Can use a regex as well
-slapp.message(/^(thanks|thank you)/i, ['mention', 'direct_message'], (msg) => {
-  // You can provide a list of responses, and a random one will be chosen
-  // You can also include slack emoji in your responses
-  msg.say([
-    "You're welcome :smile:",
-    'You bet',
-    ':+1: Of course',
-    'Anytime :sun_with_face: :full_moon_with_face:'
-  ])
-})
-
-// demonstrate returning an attachment...
-slapp.message('attachment', ['mention', 'direct_message'], (msg) => {
-  msg.say({
-    text: 'Check out this amazing attachment! :confetti_ball: ',
-    attachments: [{
-      text: 'Slapp is a robust open source library that sits on top of the Slack APIs',
-      title: 'Slapp Library - Open Source',
-      image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-      title_link: 'https://beepboophq.com/',
-      color: '#7CD197'
-    }]
-  })
-})
-
-// Catch-all for any other responses not handled above
-slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
-  // respond only 40% of the time
-  if (Math.random() < 0.4) {
-    msg.say([':wave:', ':pray:', ':raised_hands:'])
-  }
-})
+// var HELP_TEXT = `
+// I will respond to the following messages:
+// \`help\` - to see this message.
+// \`hi\` - to demonstrate a conversation that tracks state.
+// \`thanks\` - to demonstrate a simple response.
+// \`<type-any-other-text>\` - to demonstrate a random emoticon response, some of the time :wink:.
+// \`attachment\` - to see a Slack attachment message.
+// `
+//
+// // response to the user typing "help"
+// slapp.message('help', ['mention', 'direct_message'], (msg) => {
+//   msg.say(HELP_TEXT)
+// })
+//
+// // "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
+// slapp
+//   .message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
+//     msg
+//       .say(`${text}, how are you?`)
+//       // sends next event from user to this route, passing along state
+//       .route('how-are-you', { greeting: text })
+//   })
+//   .route('how-are-you', (msg, state) => {
+//     var text = (msg.body.event && msg.body.event.text) || ''
+//
+//     // user may not have typed text as their next action, ask again and re-route
+//     if (!text) {
+//       return msg
+//         .say("Whoops, I'm still waiting to hear how you're doing.")
+//         .say('How are you?')
+//         .route('how-are-you', state)
+//     }
+//
+//     // add their response to state
+//     state.status = text
+//
+//     msg
+//       .say(`Ok then. What's your favorite color?`)
+//       .route('color', state)
+//   })
+//   .route('color', (msg, state) => {
+//     var text = (msg.body.event && msg.body.event.text) || ''
+//
+//     // user may not have typed text as their next action, ask again and re-route
+//     if (!text) {
+//       return msg
+//         .say("I'm eagerly awaiting to hear your favorite color.")
+//         .route('color', state)
+//     }
+//
+//     // add their response to state
+//     state.color = text
+//
+//     msg
+//       .say('Thanks for sharing.')
+//       .say(`Here's what you've told me so far: \`\`\`${JSON.stringify(state)}\`\`\``)
+//     // At this point, since we don't route anywhere, the "conversation" is over
+//   })
+//
+// // Can use a regex as well
+// slapp.message(/^(thanks|thank you)/i, ['mention', 'direct_message'], (msg) => {
+//   // You can provide a list of responses, and a random one will be chosen
+//   // You can also include slack emoji in your responses
+//   msg.say([
+//     "You're welcome :smile:",
+//     'You bet',
+//     ':+1: Of course',
+//     'Anytime :sun_with_face: :full_moon_with_face:'
+//   ])
+// })
+//
+// // demonstrate returning an attachment...
+// slapp.message('attachment', ['mention', 'direct_message'], (msg) => {
+//   msg.say({
+//     text: 'Check out this amazing attachment! :confetti_ball: ',
+//     attachments: [{
+//       text: 'Slapp is a robust open source library that sits on top of the Slack APIs',
+//       title: 'Slapp Library - Open Source',
+//       image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
+//       title_link: 'https://beepboophq.com/',
+//       color: '#7CD197'
+//     }]
+//   })
+// })
+//
+// // Catch-all for any other responses not handled above
+// slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
+//   // respond only 40% of the time
+//   if (Math.random() < 0.4) {
+//     msg.say([':wave:', ':pray:', ':raised_hands:'])
+//   }
+// })
 
 //*********************************************
 // Server
